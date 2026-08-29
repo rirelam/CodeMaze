2385d9d00708f2b14b79b35789daa68bc3049c34
rirelam <ricardo.lamar@gmail.com>
2026-08-28 21:21:42 -0400

    pruebas de middleware

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+namespace CompanyEmployees.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+}
--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -1,33 +1,42 @@
-using CompanyEmployees.Extensions;
-using Microsoft.AspNetCore.HttpOverrides;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.ConfigureCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.MapOpenApi();
-} else
-{
-    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.All
-});
 
-app.UseCors("CorsPolicy");
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logic before executing the next delegate in the Use method"); await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine("Map branch logic in the Use method before the next delegate");
+        await next.Invoke();
+        Console.WriteLine("Map branch logic in the Use method after the next delegate");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Map branch response to the client in the Run method"); 
+        await context.Response.WriteAsync("Hello from the map branch.");
+    });
+});
+app.Run(async context =>
+{
+    Console.WriteLine($"Writing the response to the client in the Run method"); await context.Response.WriteAsync("Hello from the middleware component.");
+});
 
 var summaries = new[]
 {
